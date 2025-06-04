25fa13049588fb2de3513480affca252e1b30164
emannoel-2023 <emannoel.2023@gmail.com>
2025-06-04 18:51:47 -0300

    update capturas de teclado

--- chip8.c
+++ chip8.c
@@ -4,12 +4,14 @@
 #include <stdbool.h>
 #include "SDL.h"
 
-
+// SDL CONTAINER OBJETO
 typedef struct {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 } sdl_t;
 
+
+// EMULADOR CONFIGURACAO OBJETO
 typedef struct {
 	uint32_t window_width;
 	uint32_t window_height;
@@ -17,6 +19,22 @@ typedef struct {
 	uint32_t bg_color;
 	uint32_t scale_factor;
 } config_t;
+
+
+// ESTADOS DO EMULADOR
+typedef enum {
+	QUIT,
+	RUNNING,
+	PAUSED,
+
+
+} emulator_state_t;
+
+//CHIP8 MAQUINA OBJETO
+typedef struct {
+	emulator_state_t state;
+} chip8_t;
+
 
 // inicio SDL
 bool init_sdl(sdl_t *sdl, const config_t config){
@@ -66,7 +84,11 @@ bool set_config_from_args(config_t *config, const int argc, char **argv) {
 
 }
 
+bool init_chip8(chip8_t *chip8){
+	chip8->state = RUNNING;
+	return true;
 
+}
 
 //fim do programa
 void final_cleanup(const sdl_t sdl){
@@ -92,6 +114,35 @@ void update_screen(const sdl_t sdl){
 	SDL_RenderPresent(sdl.renderer);
 }
 
+void handle_input(chip8_t *chip8){
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)){
+		switch (event.type) {
+			case SDL_QUIT:
+				// Sair da janela; Fim do programa
+				chip8->state = QUIT;
+				return;
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym){
+					case SDLK_ESCAPE:
+						//escape key; sair da janela e fechar programa
+						chip8->state = QUIT;
+						return;
+					default:
+						break;
+
+				}
+
+			case SDL_KEYUP:
+				break;
+			default:
+				break;
+		}		
+	}
+
+}
+
 int main(int argc, char **argv) {
 	config_t config = {0};
 	if (!set_config_from_args(&config, argc, argv)) exit(EXIT_FAILURE);
@@ -100,12 +151,23 @@ int main(int argc, char **argv) {
 	sdl_t sdl = {0};
 	if (!init_sdl(&sdl, config)) exit(EXIT_FAILURE);
 	
+	// Iniciando maquina chip8
+	chip8_t chip8 = {0};
+	if (!init_chip8(&chip8)) exit(EXIT_FAILURE);
+
+
 	// limpeza inicial da tela para a cor do background
 	clear_screen(sdl, config);
 
 
 	// main loop do emulador
-	while(true){
+	while(chip8.state != QUIT){
+		//input do usuario
+		handle_input(&chip8);
+		//if 
+		// get time()
+		// instrucoes do emulador de chip8
+		// Get_time() elapsed since last get_time()
 		SDL_Delay(16);
 		update_screen(sdl);
 
