fff3097876ed791f734a8f76bb0e2b8d1c40e151
emannoel-2023 <emannoel.2023@gmail.com>
2025-06-04 11:24:03 -0300

    update chip8.c file

--- chip8.c
+++ chip8.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "SDL.h"
+
+
+// inicio SDL
+bool init_sdl(void){
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) !=0){
+		SDL_Log("Impossivel iniciar o SDL subsystems! %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true; //sucesso
+}
+
+//fim do programa
+void final_cleanup(void){
+	SDL_Quit(); // desligando SDL subsystems
+}
 
 int main(int argc, char **argv) {
 	(void)argc;
 	(void)argv;
-	puts("TESTING ON WINDOWS");
-	return 0;
+
+	// Iniciando o SDL
+	if (!init_sdl()) exit(EXIT_FAILURE);
+	
+	// final cleanup
+	final_cleanup();
+
+	exit(EXIT_SUCCESS);
 }
